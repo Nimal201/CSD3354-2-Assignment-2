099cad9979da2bd75ae50459f9c70aaa8e2e9264
visualstudio <visualstudio@122-PC19>
2019-03-08 20:34:20 -0500

    Lab06

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -6,29 +6,41 @@ using System.Threading.Tasks;
 
 //---------- Nirmal Patel------- C0730194-------
 //-----------Harpreet Singh------C0727128-------
+
 namespace DelegatesAndEvents
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             DelegateExercises de = new DelegateExercises();
-            de.Method3();
+            de.Method3(10);
             Console.Read();
         }
     }
-    public delegate void MyDelegate();
+
     public class DelegateExercises
     {
-        void Method1()
+        public delegate int MyDelegate(int i);
+        public int Method1(int intMethod1)
         {
-            System.Console.WriteLine("Method1");
+            return intMethod1 * 2;
         }
-        public void Method3()
+        public int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            System.Console.WriteLine(result);
+        }
+        public void Method3(int intMethod3)
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
-
 }
